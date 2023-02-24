01395dc27e979279a5bf6713d63230246f3be7e0
Paul Angelo Sy <84886125+sypaulangelo@users.noreply.github.com>
2023-02-24 21:08:12 +0800

    Add files via upload

---
+++ Lab 1/Lab 1/secondView.swift
@@ -0,0 +1,15 @@
+//
+//  secondView.swift
+//  Lab 1
+//
+//  Created by Paul Angelo Sy on 2/24/23.
+//
+
+import Foundation
+import UIKit
+
+class secondView: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
